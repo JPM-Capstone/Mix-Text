31d5cc211a774337a1ed64b303bd4eed04fee376
YuhangQiu00 <yq2347@columbia.edu>
2023-04-18 16:48:37 +0000

    add configs, yml, and sbatch

---
+++ mixtext_jobs/scripts/schedule_jobs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sbatch ./mixtext_jobs/sbatch/job1.sbatch
+sbatch ./mixtext_jobs/sbatch/job2.sbatch
+sbatch ./mixtext_jobs/sbatch/job3.sbatch
+sbatch ./mixtext_jobs/sbatch/job4.sbatch
+sbatch ./mixtext_jobs/sbatch/job5.sbatch
